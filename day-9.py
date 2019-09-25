6abf05ed58e3d73c45248505b44beaae5d1edf5e
Matt Williams <matt.seongjun@gmail.com>
2019-09-25 12:40:04 -0400

    Incomplete solution
    Need to account for the possibility that the largest sum is could be just from index 0 and index 4 in a len 5 array.

--- day-9.py
+++ day-9.py
@@ -1,6 +1,6 @@
 # Given a list of integers, write a function that
 # returns the largest sum of non-adjacent numbers.
-# Numbers can be 0 or negative.
+# Numbers can be 0 or negative. <===== Key
 
 # For example, [2, 4, 6, 2, 5] should return 13, 
 # since we pick 2, 6, and 5. 
@@ -8,3 +8,25 @@
 # since we pick 5 and 5.
 
 # Follow-up: Can you do this in O(N) time and constant space?
+
+def non_adj_sum(array):
+    even_sum = 0
+    odd_sum = 0
+    for i in range(len(array)):
+        if i % 2 == 0:
+            even_sum += array[i]
+        else:
+            odd_sum += array[i]
+        print("Odd:", odd_sum)
+        print("Even:", even_sum)
+
+    if odd_sum > even_sum:
+        return odd_sum
+    else:
+        return even_sum
+
+test_odd = [2, 4, 6, 2, 5]
+test_even = [5, 1, 1, 5]
+
+print(non_adj_sum(test_odd))
+print(non_adj_sum(test_even))
